96c915173e8dc2772dd406e5db0a493edfa70963
Sarim Shah <48493030+cozyrimz@users.noreply.github.com>
2021-06-22 03:45:53 -0500

    changed theme layout

---
+++ src/theme/theme.ts
@@ -0,0 +1,22 @@
+import { createMuiTheme } from '@material-ui/core/styles'
+import common from '@material-ui/core/colors/common'
+const { black, white } = common
+
+export const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: white,
+    },
+    secondary: {
+      main: black,
+    },
+  },
+  typography: {
+    // fontFamily: ['Bebas Neue', 'cursive'].join(','),
+    htmlFontSize: 16,
+    fontSize: 14,
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+    fontWeightMedium: 500,
+  },
+})
